4f48a32968597ae52a473d53a13c49c312309585
HiHighs <degroote.jonas@hotmail.com>
2024-03-11 16:58:23 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import { useState } from 'react';
 import './App.css';
+import { _flashcards } from './flashcards';
 
 export default function App() {
+  const [flashcards, setFlashcards] = useState(_flashcards);
+
+  function shuffleArray() {
+    const array = [...flashcards];
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    setFlashcards(array);
+  }
+
   return (
     <>
-      <h1>Flashcards</h1>
-      <p>Flashcards</p>
+      <div className='heading'>
+        <h1>Which array method to use? " I WANT..."</h1>
+        <button className='shuffle' onClick={shuffleArray}>
+          shuffle
+        </button>
+      </div>
+
+      <div className='flashcards'>
+        {flashcards.map((flashcard) => (
+          <Flashcard
+            key={flashcard.key}
+            question={flashcard.question}
+            answer={flashcard.answer}
+          />
+        ))}
+      </div>
     </>
+  );
+}
+
+function Flashcard({ question, answer }) {
+  const [selected, setSelected] = useState(false);
+  const [content, setContent] = useState(question);
+
+  function handleClick() {
+    setSelected(!selected);
+    setContent(selected ? question : answer);
+  }
+
+  return (
+    <div onClick={handleClick} className={selected ? 'selected' : ''}>
+      {content}
+    </div>
   );
 }
---
+++ src/flashcards.js
@@ -0,0 +1,132 @@
+export const _flashcards = [
+  // To mutate the original array
+  {
+    key: 1,
+    question: 'To add element to the original array (end)',
+    answer: '.push',
+  },
+  {
+    key: 2,
+    question: 'To add element to the original array (start)',
+    answer: '.unshift',
+  },
+  {
+    key: 3,
+    question: 'To remove element from the original array (end)',
+    answer: '.pop',
+  },
+  {
+    key: 4,
+    question: 'To remove element from the original array (start)',
+    answer: '.shift',
+  },
+  {
+    key: 5,
+    question: 'To remove element from the original array (any)',
+    answer: '.splice',
+  },
+  {
+    key: 6,
+    question: 'To reverse the original array',
+    answer: '.reverse',
+  },
+  {
+    key: 7,
+    question: 'To sort the original array',
+    answer: '.sort',
+  },
+  {
+    key: 8,
+    question:
+      'To change all elements within a range of indices in an array to a static value',
+    answer: '.fill',
+  },
+  // A new array
+  {
+    key: 9,
+    question: 'To compute a new array from original (loop)',
+    answer: '.map',
+  },
+  {
+    key: 10,
+    question: 'To filter the array using condition',
+    answer: '.filter',
+  },
+  {
+    key: 11,
+    question: 'To get a portion of the original',
+    answer: '.slice',
+  },
+  {
+    key: 12,
+    question: 'To merge 2 or more arrays',
+    answer: '.concat',
+  },
+  {
+    key: 13,
+    question: 'To get a new array with all sub-arrays flattened',
+    answer: '.flat',
+  },
+  {
+    key: 14,
+    question:
+      'To get a new array formed by applying a callback function to each element',
+    answer: '.flatMap',
+  },
+  // An array index
+  {
+    key: 15,
+    question: 'To get the index based on value',
+    answer: '.indexOf',
+  },
+  {
+    key: 16,
+    question: 'To get the index based on a test condition',
+    answer: '.findIndex',
+  },
+  // An array element
+  {
+    key: 17,
+    question: 'To get an element based on a test condition',
+    answer: '.find',
+  },
+  // Know if array includes
+  {
+    key: 18,
+    question: 'To know if an array contains a value',
+    answer: '.includes',
+  },
+  {
+    key: 19,
+    question: 'To test whether at least one element passes a test condition',
+    answer: '.some',
+  },
+  {
+    key: 20,
+    question: 'To test whether all elements pas a test condition',
+    answer: '.every',
+  },
+  // A new string
+  {
+    key: 21,
+    question: 'To get a new string based on a seperator string',
+    answer: '.join',
+  },
+  // To transform to value
+  {
+    key: 22,
+    question: 'To transform to a value based on an accumulator',
+    answer: '.reduce',
+  },
+  // To just loop array
+  {
+    key: 23,
+    question: 'To simply loop the array based on a callback',
+    answer: '.forEach',
+  },
+  {
+    key: 24,
+    question: 'Monkey loves you',
+    answer: 'Monkey needs a hug',
+  },
+];
